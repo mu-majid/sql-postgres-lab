ff3f892bbfd9fca3b79e84258403f450cc041d26
Muhammad Majid <mmajidkamel@gmail.com>
2021-03-14 19:42:20 +0200

    added queries notes file

---
+++ 4.instagram-db-clone/queries.sql
@@ -0,0 +1,22 @@
+-- listing the most recent 3 users in our DB
+SELECT *
+FROM users
+ORDER BY id DESC
+LIMIT 3;
+
+----------
+
+-- Posts created by a particular user
+SELECT p.caption, u.username
+FROM users AS u
+JOIN posts as p
+  ON p.user_id = u.id
+WHERE u.id = 200;
+
+----------
+
+-- show usernames and number of likes each user created
+SELECT username, COUNT(*)
+FROM users
+JOIN likes ON likes.user_id = users.id
+GROUP BY username;
