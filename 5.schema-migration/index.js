338c5bd7155827995bcd41115a7c7ffecf345e34
Muhammad Majid <mmajidkamel@gmail.com>
2021-03-28 17:20:23 +0200

    Step 2: Deploy a new verison of our server that insert loc and lng, lat

--- 5.schema-migration/index.js
+++ 5.schema-migration/index.js
@@ -58,7 +58,9 @@ app.get('/posts', async (req, res) => {
 app.post('/posts', async (req, res) => {
   const { lng, lat } = req.body;
 
-  await pool.query('INSERT INTO posts (lat, lng) VALUES ($1, $2);', [lat, lng]);
+  await pool.query('INSERT INTO posts (lat, lng, loc) VALUES ($1, $2, $3);',
+  [lat, lng, `(${lng},${lat})`]
+  );
 
   res.redirect('/posts');
 });
