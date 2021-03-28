b7f56e7d2b99a0bf6b8a50cce790f5c901516910
Muhammad Majid <mmajidkamel@gmail.com>
2021-03-28 17:09:45 +0200

    simple web app to interact with posts table

---
+++ 5.schema-migration/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const pg = require('pg');
+
+const pool = new pg.Pool({
+  host: 'localhost',
+  port: 5432,
+  database: 'socialnetwork',
+  user: 'majid',
+  password: '',
+});
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/posts', async (req, res) => {
+  const { rows } = await pool.query(`
+    SELECT * FROM posts;
+  `);
+
+  res.send(`
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>lng</th>
+          <th>lat</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${rows
+          .map((row) => {
+            return `
+            <tr>
+              <td>${row.id}</td>
+              <td>${row.lng}</td>
+              <td>${row.lat}</td>
+            </tr>
+          `;
+          })
+          .join('')}
+      </tbody>
+    </table>
+    <form method="POST">
+      <h3>Create Post</h3>
+      <div>
+        <label>Lng</label>
+        <input name="lng" />
+      </div>
+      <div>
+        <label>Lat</label>
+        <input name="lat" />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  `);
+});
+
+app.post('/posts', async (req, res) => {
+  const { lng, lat } = req.body;
+
+  await pool.query('INSERT INTO posts (lat, lng) VALUES ($1, $2);', [lat, lng]);
+
+  res.redirect('/posts');
+});
+
+app.listen(3005, () => {
+  console.log('Listening on port 3005');
+});
