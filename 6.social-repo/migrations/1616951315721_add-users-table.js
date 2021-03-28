23efe5d87f1ab48ca09d25e7483f3ec14b0327ea
Muhammad Majid <mmajidkamel@gmail.com>
2021-03-28 19:10:33 +0200

    WIP accessing DB from API server patterns

---
+++ 6.social-repo/migrations/1616951315721_add-users-table.js
@@ -0,0 +1,26 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+  pgm.sql(
+    `
+    CREATE TABLE posts (
+      id SERIAL PRIMARY KEY,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      bio VARCHAR(400),
+      username VARCHAR(30) NOT NULL
+    );
+    `
+  );
+};
+
+exports.down = pgm => {
+  pgm.sql(
+    `
+    DROP TABLE users;
+    
+    `
+  );
+};
