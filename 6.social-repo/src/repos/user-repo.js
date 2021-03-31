6412c746d9773e2abdad75f99eeffe6c3c9aeaa6
Muhammad Majid <mmajidkamel@gmail.com>
2021-03-31 21:53:16 +0200

    Setup for failing parallel testing

--- 6.social-repo/src/repos/user-repo.js
+++ 6.social-repo/src/repos/user-repo.js
@@ -43,6 +43,12 @@ class UserRepo {
 
     return toCamelCase(rows)[0];
   }
+
+  static async count() {
+    const { rows } = await pool.query('SELECT COUNT(*) FROM users;');
+
+    return parseInt(rows[0].count);
+  }
 }
 
 module.exports = UserRepo;
